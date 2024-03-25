651a930ac22e6be45178c64455a40d90e9e0f1cd
swapnilhworld <swapnilhworld@gmail.com>
2024-03-25 15:41:13 +0530

    first commit from ide

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -12,13 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
+	
+	@Autowired
+	private CurrencyExchangeRepository repo;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to) {
-		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+//		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = repo.findByFromAndTo(from, to);
+		if(currencyExchange == null)
+			throw new RuntimeException("unable to find data for " + from+" to " + to);
 		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+		
 		return currencyExchange;
 	}
 
